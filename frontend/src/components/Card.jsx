16a55bef0d7dd8d4d32abe67f65aa4b7d7544596
Aarize29 <1ds21ai045@dsce.edu.in>
2024-01-22 23:11:41 +0530

    slight change

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -137,7 +137,7 @@ import {
             </Typography>
             <Input
               size="lg"
-              value={`https://shorturl/${shorturl}`}
+              value={`http://locahost:3000/${shorturl}`}
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
